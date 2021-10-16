260990f753e9ee37bef2a14be98018a2e0af46ed
Richard Machado <mmachado.richard@gmail.com>
2021-10-15 19:14:29 -0700

    runs prettier on profile model and router

--- profile/profile-model.js
+++ profile/profile-model.js
@@ -1,4 +1,4 @@
-const database = require("../data/dbConfig");
+const database = require("../data/dbConfig.js");
 
 const add = (profile) => {
   return database("profile").insert(profile).returning("id");
